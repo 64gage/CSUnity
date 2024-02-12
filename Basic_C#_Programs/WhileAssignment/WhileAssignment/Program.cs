a534662bc2bb3e3c93545dd97d209a5e8cd9c6b9
64gage <138918934+64gage@users.noreply.github.com>
2024-02-11 22:07:35 -0600

    Console App Do While Assignment

---
+++ Basic_C#_Programs/WhileAssignment/WhileAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WhileAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number between 1 and 100!");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = false;
+
+            do
+            {
+                switch (number)
+                {
+                    case 64:
+                        Console.WriteLine("You guessed the number 64. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine(number + " is incorrect. Guess again?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+
+            Console.ReadLine();
+        }
+    }
+}
