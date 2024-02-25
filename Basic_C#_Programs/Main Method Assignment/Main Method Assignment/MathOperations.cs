c5f326f7e960569fcccb8f6a37d1ce2ede1ab830
64gage <138918934+64gage@users.noreply.github.com>
2024-02-25 03:22:45 -0600

    Upload Main Methods Assignment

---
+++ Basic_C#_Programs/Main Method Assignment/Main Method Assignment/MathOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main_Method_Assignment
+{
+    public class MathOperations
+    {
+        public void PerformOperation(int num1, int num2)
+        {
+            int result = num1 * 2;
+
+            Console.WriteLine("Second Integer: " + num2);
+
+            Console.WriteLine("Result of the operation on the first integer: " + result);
+        }
+    }
+}
---
+++ Basic_C#_Programs/Main Method Assignment/Main Method Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Main_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOps = new MathOperations();
+
+            mathOps.PerformOperation(5, 10);
+
+            mathOps.PerformOperation(num1: 8, num2: 20);
+        }
+    }
+}
