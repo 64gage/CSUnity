0c27f525b3f730523b3fdeb7816aa0f75608e849
64gage <138918934+64gage@users.noreply.github.com>
2024-02-25 11:17:07 -0600

    Operators assignment
    Wrote code for Operators Assignment within Methods and Objects Assignment

--- Basic_C#_Programs/Methods and Objects Assignment/Methods and Objects Assignment/Employee.cs
+++ Basic_C#_Programs/Methods and Objects Assignment/Methods and Objects Assignment/Employee.cs
@@ -9,6 +9,22 @@ namespace Methods_and_Objects_Assignment
         //Property
         public int Id { get; set; }
 
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (ReferenceEquals(employee1, employee2))
+                return true;
+
+            if (employee1 is null || employee2 is null)
+                return false;
+
+            return employee1.Id == employee2.Id;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return !(employee1 == employee2);
+        }
+
         // Constructor
         public Employee(string firstName, string lastName, int id) : base(firstName, lastName)
         {
--- Basic_C#_Programs/Methods and Objects Assignment/Methods and Objects Assignment/Program.cs
+++ Basic_C#_Programs/Methods and Objects Assignment/Methods and Objects Assignment/Program.cs
@@ -7,10 +7,20 @@ namespace Methods_and_Objects_Assignment
         static void Main(string[] args)
         {
             //Instantiate and initialize employee object
-            Employee employee = new Employee("Sample", "Student", 123);
+            Employee employee1 = new Employee("John", "Doe", 123);
+            Employee employee2 = new Employee("Jane", "Doe", 124);
+
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("Employees have the smae Id.");
+            }
+            else
+            {
+                Console.WriteLine("Employees have different Ids.");
+            }
 
             //Call superclass method SayName()
-            employee.SayName();
+            employee1.SayName();
         }
     }
 }
