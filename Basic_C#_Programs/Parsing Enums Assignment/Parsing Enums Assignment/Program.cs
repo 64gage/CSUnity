f4c74945f66b6ce5be03e43020ccc987124e3467
64gage <138918934+64gage@users.noreply.github.com>
2024-02-25 12:17:13 -0600

    Parsing Enums assignment

---
+++ Basic_C#_Programs/Parsing Enums Assignment/Parsing Enums Assignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Parsing_Enums_Assignment
+{
+    class Program
+    {
+        enum DayOfWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Please enter the current day of the week: ");
+                string userInput = Console.ReadLine();
+                DayOfWeek currentDay;
+
+                //Attempt to parse the user input into DayOfWeek enum
+                if (Enum.TryParse(userInput, out currentDay))
+                {
+                    Console.WriteLine($"You entered: {currentDay}");
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid day of the week.");
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+        }
+    }
+}
