f8c0af4917fbff18e65ecb004b6f83ee4b7bf220
64gage <138918934+64gage@users.noreply.github.com>
2024-02-28 11:38:50 -0600

    DateTime assignment

---
+++ Basic_C#_Programs/DateTime Assignment/DateTime Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine("Current date and time: " + currentDateTime);
+            Console.Write("Enter a number of hours: ");
+
+            int hours = Convert.ToInt32(Console.ReadLine());
+            DateTime futureDateTime = currentDateTime.AddHours(hours);
+
+            Console.WriteLine($"In {hours} hours, it will be: {futureDateTime}");
+        }
+    }
+}
